8ca3ff4a8fa0b260f10f9cc7de13a637389a9e66
IB <ilyess.batal7@gmail.com>
2024-05-09 11:51:07 +0200

    swagger ui

--- src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
+++ src/main/java/com/dauphine/blogger/BloggerBoxBackendApplication.java
@@ -3,7 +3,20 @@ package com.dauphine.blogger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Blogger box backend",
+				description = "Blogger box endpoints and Api",
+				contact = @Contact(name = "ilyess", email="ilyess.batal7@gmail.com"),
+				version = "1.0.0"
+				)
+)
+
 public class BloggerBoxBackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
+++ src/main/java/com/dauphine/blogger/controllers/HelloWorldController.java
@@ -4,7 +4,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(
+		name = "Hello world API",
+		description = "My first hello world endpoints"
+)
 public class HelloWorldController
 {
 	
@@ -15,7 +23,10 @@ public class HelloWorldController
 	}
 	
 	@GetMapping("hello/{name}")
-	public String helloByName(@PathVariable String name)
+	@Operation
+	public String helloByName(
+			@Parameter(description = "Name to greet")
+			@PathVariable String name)
 	{
 		return "Hello " + name;
 	}
